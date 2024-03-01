517047dd83e5e26b20c4232e6139339619bef254
Isokjon-Osimjonov <john@Johns-Mac.local>
2024-03-01 20:24:03 +0900

    router

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,13 +4,13 @@ import CLOGO from "../../assets/logocart.svg";
 const Home = () => {
   return (
     <div className="home_main">
-      <NavLink to="/catalog/drinks" className="catalog_category">
+      <NavLink to="/catalog/drinks/" className="catalog_category">
         <img className="icon" src={CLOGO} alt="" /> Ichimliklar
       </NavLink>
-      <NavLink to="/catalog/sweets" className="catalog_category">
+      <NavLink to="/catalog/sweets/" className="catalog_category">
         <img className="icon" src={CLOGO} alt="" /> Shirinliklar
       </NavLink>
-      <NavLink to="/catalog/pickles" className="catalog_category">
+      <NavLink to="/catalog/pickles/" className="catalog_category">
         <img className="icon" src={CLOGO} alt="" /> Tuzlamalar
       </NavLink>
       <NavLink to="/catalog/meats" className="catalog_category">
