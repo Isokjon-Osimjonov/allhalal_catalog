4c6ce84ef871d87814768df3fea1dc21f44281df
Isokjon-Osimjonov <john@Johns-Mac.local>
2024-03-01 20:11:29 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  // useRoutes,
 } from "react-router-dom";
 
 import AppLayout from "./components/app/AppLayout";
 import Home from "./components/home/Home";
 import Catalog from "./components/catalog/Catalog";
 
-// Define the routes array with at least one route configuration
-const router = createBrowserRouter([{
-  element: <AppLayout />,
-  children: [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/:type",
-      element: <Catalog />,
-    },
-  ],
-}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />, // Render the AppLayout component
+    children: [
+      {
+        index: true,
+        element: <Home />, // Render the Home component at the root path
+      },
+      {
+        path: "/catalog/:type",
+        element: <Catalog />, // Render the Catalog component for /catalog/:type paths
+      },
+    ],
+  },
+]);
 
 function App() {
-  // const element = useRoutes(router);
   return <RouterProvider router={router}></RouterProvider>;
 }
 
